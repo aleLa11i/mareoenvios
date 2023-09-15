286bf1d0a0f5f74bdeea9663018c31a5c28d1f93
Alesandro Lalli <alesandro.lalli@gmail.com>
2023-09-15 10:25:34 -0300

    first commit

---
+++ src/main/java/sube/interviews/mareoenvios/MareoEnviosApplication.java
@@ -0,0 +1,14 @@
+package sube.interviews.mareoenvios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MareoEnviosApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MareoEnviosApplication.class, args);
+	}
+
+}
