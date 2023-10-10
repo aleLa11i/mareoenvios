b0d82cf67d3d554e5f0ef264597266095e31e75b
Alesandro Lalli <alesandro.lalli@gmail.com>
2023-10-10 17:19:03 -0300

    Se acomoda codigo/ masTimeTask se valoriza por props

--- src/main/java/sube/interviews/mareoenvios/bo/impl/ConcurrentTaskBoImpl.java
+++ src/main/java/sube/interviews/mareoenvios/bo/impl/ConcurrentTaskBoImpl.java
@@ -1,13 +1,13 @@
 package sube.interviews.mareoenvios.bo.impl;
 
-import jakarta.persistence.NoResultException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import sube.interviews.mareoenvios.bo.ConcurrentTaskBO;
 import sube.interviews.mareoenvios.dto.ConcurrentTaskRequestDTO;
-import sube.interviews.mareoenvios.dto.ConcurrentTaskShippingDTO;
 import sube.interviews.mareoenvios.entity.Shipping;
 import sube.interviews.mareoenvios.entity.Task;
 import sube.interviews.mareoenvios.enums.ShippingStateEnum;
@@ -28,7 +28,9 @@ import java.util.concurrent.*;
 public class ConcurrentTaskBoImpl implements ConcurrentTaskBO {
 
     private final static Logger LOGGER = LogManager.getLogger(ConcurrentTaskBoImpl.class);
-    private final static Long maxTimeTask = 60L; //Se establece un maximo de 60 segundos para cada tarea
+
+    @Value("${max-time-task}")
+    private String maxTimeTask;
 
     @Autowired
     ShippingRepository shippingRepository;
@@ -46,7 +48,7 @@ public class ConcurrentTaskBoImpl implements ConcurrentTaskBO {
                 Shipping shipping = shippingRepository.getById(taskShippingDTO.getShippingId());
                 Task task = taskRepository.save(new Task(shipping, TaskStateEnum.PENDING.getValue(), LocalDateTime.now(), taskShippingDTO.getNextState(), taskShippingDTO.getTimeStartInSeg()));
 
-                if(taskShippingDTO.getTimeStartInSeg() > maxTimeTask){
+                if(taskShippingDTO.getTimeStartInSeg() > Integer.parseInt(maxTimeTask)){
                     cancelTask(task,String.format("La tarea del envío con ID='%s' supera el tiempo maximo de %s segundos.",  shipping.getId(), maxTimeTask));
                     return;
                 }
@@ -56,38 +58,30 @@ public class ConcurrentTaskBoImpl implements ConcurrentTaskBO {
                     return;
                 }
 
-                Thread shippingTask = new Thread(() -> {
+                new Thread(() -> {
                     ExecutorService executor = Executors.newSingleThreadExecutor();
                     try {
                         Future<?> future;
-                        future = executor.submit( () ->{
-                            try {
-                                this.executeTask( task.getTimeStart(), task.getNextState(), task.getShippingId(), task);
-                            } catch (BusinessException e) {
-                                e.printStackTrace();
-                            }
-                        });
-                        future.get();
+                        Task actualTask = task;
                         do{
-                            Task actualTask = taskRepository.getPendingTaskList(shipping).get(0);
+                            Task finalActualTask = actualTask; //Se debe crear una variables actualTask final para poder utilizarla dentro de método runnable
                             future = executor.submit(() -> {
                                 try {
-                                    this.executeTask( actualTask.getTimeStart(), actualTask.getNextState(), actualTask.getShippingId(), actualTask);
+                                    this.executeTask( finalActualTask.getTimeStart(), finalActualTask.getNextState(), finalActualTask.getShippingId(), finalActualTask);
                                 } catch (BusinessException e) {
                                     e.printStackTrace();
                                 }
                             });
-                            future.get();
-                            //TODO: Agregar timeout
+                            future.get( Integer.parseInt(maxTimeTask), TimeUnit.SECONDS);
+                            actualTask = taskRepository.getPendingTaskList(shipping).get(0);
+
                         }while(!taskRepository.getPendingTaskList(shipping).isEmpty());
-                    } catch (InterruptedException | ExecutionException | RepositoryException e) {
+                    } catch (InterruptedException | ExecutionException | RepositoryException | TimeoutException e) {
                         e.printStackTrace();
                     }  finally {
                         executor.shutdown();
                     }
-                });
-
-                shippingTask.start();
+                }).start();
 
             } catch (BusinessException | RepositoryException e) {
                 e.printStackTrace();
